bba0dd8cfaab74923ce3e816304fbb5182cca45f
Vinicios-Developer <viniciosgabriel_18@hotmail.com>
2024-06-19 11:00:30 -0400

    Ajustando o código para download

--- src/routes.py
+++ src/routes.py
@@ -1,52 +1,56 @@
+import pythoncom  # Importe a biblioteca pythoncom
 from docx.shared import Pt
 from flask import render_template, request, send_file
-
 from certificado.src import app
 from certificado.src.forms import FormNome
 from certificado.src.models import Nome
 from docx import Document
 from docx2pdf import convert
 import os
+import time
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    pythoncom.CoInitialize()
     form = FormNome()
     if request.method == 'POST':
         nome = request.form.get('nome')
-        user = Nome.query.filter_by(username=nome).first()
+        nome_padronizado = nome.upper()
+
+        user = Nome.query.filter_by(username=nome_padronizado).first()
         print(user)
         if user:
-            # Caminho para o template do certificado
+
             template_path = os.path.join(os.path.dirname(__file__), '..', 'CERTIFICADO_WORK.docx')
-            
-            # Cria o documento a partir do template
+
             doc = Document(template_path)
 
             for paragraph in doc.paragraphs:
                 if 'NOME' in paragraph.text:
                     paragraph.text = paragraph.text.replace('NOME', user.username)
-                    paragraph.style.font.name = 'Arial'
-                    paragraph.style.font.bold = True
-                    paragraph.style.font.size = Pt(20)
+                    for run in paragraph.runs:
+                        run.font.name = 'Arial'
+                        run.font.bold = True
+                        run.font.size = Pt(20)
 
-            # Caminhos temporários para salvar o .docx e o PDF gerado
-            temp_doc_path = 'certificate_{}.docx'.format(user.username)
-            temp_pdf_path = os.path.join(os.path.dirname(__file__), 'certificado',
-                                         'certificate_{}.pdf'.format(user.username))
+            temp_dir = os.path.join(os.path.dirname(__file__), '..', 'temp_files')
+            if not os.path.exists(temp_dir):
+                os.makedirs(temp_dir)
+
+            temp_doc_path = os.path.join(temp_dir, 'certificate_{}.docx'.format(user.username))
+            temp_pdf_path = os.path.join(temp_dir, 'certificate_{}.pdf'.format(user.username))
             doc.save(temp_doc_path)
 
-            # Converte o .docx diretamente para PDF
             convert(temp_doc_path, temp_pdf_path)
 
-            # Verifica se o arquivo PDF existe antes de enviar
+            time.sleep(4)
+
             if os.path.exists(temp_pdf_path):
                 try:
                     return send_file(temp_pdf_path, as_attachment=True)
                 except Exception as e:
-                    return str(e), 500  # Retorna o erro para depuração
-                finally:
-                    os.remove(temp_doc_path)
+                    return str(e), 500
             else:
                 return "Arquivo PDF não encontrado", 404
         else:
